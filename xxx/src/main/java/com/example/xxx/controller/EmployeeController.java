991fa67675eb3874d0818afaa3b35cb2e3081b4c
unknown <davedalimocon@gmail.com>
2023-12-31 13:46:42 +0800

    happy new year!

--- xxx/src/main/java/com/example/xxx/controller/EmployeeController.java
+++ xxx/src/main/java/com/example/xxx/controller/EmployeeController.java
@@ -1,7 +1,21 @@
 package com.example.xxx.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
+
+    @GetMapping("/")
+    public String displayName(){
+        return "hello";
+    }
+
+    @PostMapping("/add")
+    public String addName(){
+        return "Happy new year";
+    }
 }
+
+
--- xxx/src/main/java/com/example/xxx/entity/Employee.java
+++ xxx/src/main/java/com/example/xxx/entity/Employee.java
@@ -8,6 +8,63 @@ public class Employee {
     private String employeeAddress;
     private String employeeGender;
     private String employeeDepartment;
+
+    public Integer getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Integer employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getEmployeeContactNumber() {
+        return employeeContactNumber;
+    }
+
+    public void setEmployeeContactNumber(String employeeContactNumber) {
+        this.employeeContactNumber = employeeContactNumber;
+    }
+
+    public String getEmployeeAddress() {
+        return employeeAddress;
+    }
+
+    public void setEmployeeAddress(String employeeAddress) {
+        this.employeeAddress = employeeAddress;
+    }
+
+    public String getEmployeeGender() {
+        return employeeGender;
+    }
+
+    public void setEmployeeGender(String employeeGender) {
+        this.employeeGender = employeeGender;
+    }
+
+    public String getEmployeeDepartment() {
+        return employeeDepartment;
+    }
+
+    public void setEmployeeDepartment(String employeeDepartment) {
+        this.employeeDepartment = employeeDepartment;
+    }
+
+    public String getEmployeeSkills() {
+        return employeeSkills;
+    }
+
+    public void setEmployeeSkills(String employeeSkills) {
+        this.employeeSkills = employeeSkills;
+    }
+
     private String employeeSkills;
 
     public Employee(){
